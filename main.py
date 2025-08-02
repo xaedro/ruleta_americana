2587105aa8ecac654fc3bf71e8550a140ab4edbe
xaedro <mariocardona21@gmail.com>
2025-08-02 10:10:17 -0500

    Se cambia la configuracion de CORS y de iceServers.

--- main.py
+++ main.py
@@ -316,12 +316,22 @@ class PermissionsPolicyMiddleware(BaseHTTPMiddleware):
 		return response
 
 # --- MIDDLEWARE DE CORS ---
+"""
 app.add_middleware(
 	CORSMiddleware,
 	allow_origins=[
 		"https://ruletaamericana.up.railway.app",
 		"http://localhost:3000",
 	],
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
+"""
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=["https://ruletaamericana.up.railway.app"],
 	allow_credentials=True,
 	allow_methods=["*"],
 	allow_headers=["*"],
